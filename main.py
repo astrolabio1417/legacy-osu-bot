3f661667a1aa0c6c0bb3d30695c65ee81868a93e
astrolabio1417 <astrolabio1417@gmail.com>
2023-06-21 20:42:18 +0800

    remove comments

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import json
 from irc import OsuIrc
 from roombot import Room, RoomBot
 from beatmaps import RoomBeatmap
-from bot_enums import BOT_MODE, TEAM_MODE, SCORE_MODE, PLAY_MODE
+from bot_enums import BOT_MODE, TEAM_MODE
 
 with open("config.json", "r") as f:
     configuration = json.loads(f.read())
@@ -23,8 +23,8 @@ room_bot = RoomBot(irc=irc)
 room_bot.add_room(
     Room(
         irc=irc,
-        name="5.5-6.5* | AR9+ 2-4Mins | Auto Map Rotate",
-        password="",
+        name="5.5-6.5* | AR9+ 2-4Mins | Test Auto Map Rotate",
+        password="test",
         bot_mode=BOT_MODE.AUTO_ROTATE_MAP,
         team_mode=TEAM_MODE.HEAD_TO_HEAD,
         room_size=12,
@@ -36,48 +36,6 @@ room_bot.add_room(
         ),
     )
 )
-
-
-# room_bot.add_room(
-#     Room(
-#         irc=irc,
-#         # name="5.0 - 6.0* | AR9+ | 0-5Mins | Auto Host Rotate",
-#         name="testing2",
-#         password="test",
-#         bot_mode=BOT_MODE.AUTO_HOST,
-#         team_mode=TEAM_MODE.HEAD_TO_HEAD,
-#         score_mode=SCORE_MODE.SCORE,
-#         play_mode=PLAY_MODE.OSU,
-#         room_size=12,
-#         beatmap=RoomBeatmap(
-#             ar=(9.00, 10.00),
-#             star=(5.5, 6.5),
-#             length=(0, 300),
-#             bpm=(0, 500),
-#             force_stat=True,
-#         ),
-#     )
-# )
-
-# room_bot.add_room(
-#     Room(
-#         irc=irc,
-#         # name="6.0 - 7.0* | AR9+ | 0-5Mins | Auto Host Rotate",
-#         name="testing1",
-#         password="test",
-#         bot_mode=BOT_MODE.AUTO_HOST,
-#         team_mode=TEAM_MODE.HEAD_TO_HEAD,
-#         score_mode=SCORE_MODE.SCORE,
-#         play_mode=PLAY_MODE.OSU,
-#         room_size=12,
-#         beatmap=RoomBeatmap(
-#             ar=(9.00, 10.00),
-#             star=(6.0, 7.0),
-#             length=(0, 300),
-#             bpm=(0, 500),
-#         ),
-#     )
-# )
 
 ###
 ### end add room
